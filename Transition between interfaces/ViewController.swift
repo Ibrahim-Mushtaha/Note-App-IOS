ad16c861ca935ddc390c23d4e6e22f938b2c7438
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-04-12 02:59:40 -0700

    Initial Commit

---
+++ Transition between interfaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transition between interfaces
+//
+//  Created by Ibrahim Mushtaha on 12/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Transition between interfacesTests/Transition_between_interfacesTests.swift
@@ -0,0 +1,33 @@
+//
+//  Transition_between_interfacesTests.swift
+//  Transition between interfacesTests
+//
+//  Created by Ibrahim Mushtaha on 12/04/2021.
+//
+
+import XCTest
+@testable import Transition_between_interfaces
+
+class Transition_between_interfacesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

e98461d932c10a811ee15716516c7d0539865060
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-04-12 07:44:54 -0700

    finish sign in & sign up ui

--- Transition between interfaces/SignUpViewController.swift
+++ Transition between interfaces/SignUpViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var uiContanier: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        uiContanier.addCorner()
+        uiContanier.dropShadow()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uiBtnDismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
--- Transition between interfaces/ViewController.swift
+++ Transition between interfaces/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiContanier: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        uiContanier.dropShadow()
+        uiContanier.addCorner()
     }
 
-
+    @IBAction func btnCreateNew(_ sender: UIButton) {
+        performSegue(withIdentifier: "actionSequa", sender: nil)
+    }
+    
+    // to send date between the two uiController
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "actionSequa" {
+            if let details = segue.destination as? SignUpViewController{
+                
+            }
+        }
+    }
+    
 }
 
+extension UIView {
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = true
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 1
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+    
+    func addCorner(value:Int = 4){
+        layer.masksToBounds = true
+        layer.cornerRadius = 4
+    }
+}

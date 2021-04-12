736b205d74c87fe7c6b2c6445e73417c9a39dd95
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-04-12 13:32:47 -0700

    trying to add custom view

--- Transition between interfaces/HomeViewController.swift
+++ Transition between interfaces/HomeViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var uiToolbarView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        
+        let rectShape = CAShapeLayer()
+        rectShape.bounds = uiToolbarView.frame
+        rectShape.position = uiToolbarView.center
+        rectShape.path = UIBezierPath(roundedRect: uiToolbarView.bounds, byRoundingCorners: [.bottomLeft , .bottomRight], cornerRadii: CGSize(width: 12, height: 12)).cgPath
+
+        uiToolbarView.layer.backgroundColor = UIColor.orange.cgColor
+        //Here I'm masking the textView's layer with rectShape layer
+        uiToolbarView.layer.mask = rectShape
+        uiToolbarView.dropShadow()
         // Do any additional setup after loading the view.
     }
     
